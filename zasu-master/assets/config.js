f117b5b96c9997ee47d398ec356542c4e92fd669
takataka12 <taka.ns00@gmail.com>
2026-09-26 07:35:38 +0900

    Add mastering status endpoint

--- zasu-master/assets/config.js
+++ zasu-master/assets/config.js
@@ -2,6 +2,7 @@ window.ZASU_MASTER_CONFIG = {
   betaEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/submit-beta-application",
   createMixUploadEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/create-mix-upload",
   completeMixUploadEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/complete-mix-upload",
+  masteringStatusEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/mastering-status",
   supabaseUrl: "https://siwmzradvrtetotakkbi.supabase.co",
   supabasePublishableKey: "sb_publishable_-rY9u8jnlhS0Qg1XmPlqKg_ViQ0sZYn",
   betaAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd216cmFkdnJ0ZXRvdGFra2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNTk2MTEsImV4cCI6MjEwNTkzNTYxMX0.aF71_QHKtm5JxIN4h3Q-huwKBcdQEOg72fKxErINgMQ",
