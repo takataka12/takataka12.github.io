ed07eea523cca94f2eded372408d23cc456a4546
takataka12 <taka.ns00@gmail.com>
2026-09-26 06:15:54 +0900

    Configure ZASU MASTER mix upload endpoints

--- zasu-master/assets/config.js
+++ zasu-master/assets/config.js
@@ -1,4 +1,9 @@
 window.ZASU_MASTER_CONFIG = {
   betaEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/submit-beta-application",
-  betaAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd216cmFkdnJ0ZXRvdGFra2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNTk2MTEsImV4cCI6MjEwNTkzNTYxMX0.aF71_QHKtm5JxIN4h3Q-huwKBcdQEOg72fKxErINgMQ"
+  createMixUploadEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/create-mix-upload",
+  completeMixUploadEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/complete-mix-upload",
+  supabaseUrl: "https://siwmzradvrtetotakkbi.supabase.co",
+  supabasePublishableKey: "sb_publishable_-rY9u8jnlhS0Qg1XmPlqKg_ViQ0sZYn",
+  betaAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd216cmFkdnJ0ZXRvdGFra2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNTk2MTEsImV4cCI6MjEwNTkzNTYxMX0.aF71_QHKtm5JxIN4h3Q-huwKBcdQEOg72fKxErINgMQ",
+  uploadMaxBytes: 52428800
 };
