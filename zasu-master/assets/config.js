37a07d7ae5637c75b09464967412a1ac297fdd1e
takataka12 <taka.ns00@gmail.com>
2026-09-26 06:43:47 +0900

    Add Square paid beta payment link

--- zasu-master/assets/config.js
+++ zasu-master/assets/config.js
@@ -5,5 +5,6 @@ window.ZASU_MASTER_CONFIG = {
   supabaseUrl: "https://siwmzradvrtetotakkbi.supabase.co",
   supabasePublishableKey: "sb_publishable_-rY9u8jnlhS0Qg1XmPlqKg_ViQ0sZYn",
   betaAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd216cmFkdnJ0ZXRvdGFra2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNTk2MTEsImV4cCI6MjEwNTkzNTYxMX0.aF71_QHKtm5JxIN4h3Q-huwKBcdQEOg72fKxErINgMQ",
+  squarePaymentLink: "https://square.link/u/ArDEgNp3",
   uploadMaxBytes: 52428800
 };
