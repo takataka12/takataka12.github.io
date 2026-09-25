aedf7278e726d6ae00fb29650cc2d1a4cf0dbe0c
takataka12 <taka.ns00@gmail.com>
2026-09-26 07:39:41 +0900

    Prepare Railway worker endpoint configuration

--- zasu-master/assets/config.js
+++ zasu-master/assets/config.js
@@ -7,5 +7,7 @@ window.ZASU_MASTER_CONFIG = {
   supabasePublishableKey: "sb_publishable_-rY9u8jnlhS0Qg1XmPlqKg_ViQ0sZYn",
   betaAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd216cmFkdnJ0ZXRvdGFra2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNTk2MTEsImV4cCI6MjEwNTkzNTYxMX0.aF71_QHKtm5JxIN4h3Q-huwKBcdQEOg72fKxErINgMQ",
   squarePaymentLink: "https://square.link/u/ArDEgNp3",
+  workerBaseUrl: "",
+  workerUploadMaxBytes: 1073741824,
   uploadMaxBytes: 52428800
 };
