363bdf46a110f5ac756b4f3ac3e02777b3e3aa40
takataka12 <taka.ns00@gmail.com>
2026-09-26 05:17:50 +0900

    Connect ZASU MASTER beta form to Supabase

--- zasu-master/assets/config.js
+++ zasu-master/assets/config.js
@@ -1,1 +1,4 @@
-window.ZASU_MASTER_CONFIG = { betaEndpoint: "", betaEmail: "" };
+window.ZASU_MASTER_CONFIG = {
+  betaEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/submit-beta-application",
+  betaAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpd216cmFkdnJ0ZXRvdGFra2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNTk2MTEsImV4cCI6MjEwNTkzNTYxMX0.aF71_QHKtm5JxIN4h3Q-huwKBcdQEOg72fKxErINgMQ"
+};
