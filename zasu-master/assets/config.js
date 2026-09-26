1d6d960935a158f6dfd2b0541cec926593ac9727
takataka12 <taka.ns00@gmail.com>
2026-09-26 12:53:23 +0900

    Add dynamic Square checkout endpoint

--- zasu-master/assets/config.js
+++ zasu-master/assets/config.js
@@ -1,5 +1,6 @@
 window.ZASU_MASTER_CONFIG = {
   betaEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/submit-beta-application",
+  createSquareCheckoutEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/create-square-checkout",
   createMixUploadEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/create-mix-upload",
   completeMixUploadEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/complete-mix-upload",
   masteringStatusEndpoint: "https://siwmzradvrtetotakkbi.supabase.co/functions/v1/mastering-status",
